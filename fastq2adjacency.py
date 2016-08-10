b47be7f4123d0c9259c954b27a4944e0eeeea5fe
Mark McDowall <mark.mcdowall@gmail.com>
2016-08-10 16:23:34 +0100

    Added functions for merging adjacency matrixes, calcualting TADs and loading saved arrays

--- fastq2adjacency.py
+++ fastq2adjacency.py
@@ -1,4 +1,4 @@
-import os, os.path
+import os, os.path, urllib2
 
 import pytadbit
 from pytadbit.mapping               import get_intersection
@@ -7,6 +7,7 @@ from pytadbit.mapping.filter        import filter_reads
 from pytadbit.mapping.mapper        import full_mapping
 from pytadbit.parsers.map_parser    import parse_map
 from pytadbit.parsers.hic_parser    import load_hic_data_from_reads
+from pytadbit.parsers.hic_parser    import read_matrix
 from pytadbit.parsers.genome_parser import parse_fasta
 from pytadbit.utils.file_handling   import mkdir
 import numpy as np
@@ -14,7 +15,7 @@ import numpy as np
 
 class fastq2adjacency:
     """
-    These are the parts of teh TADbit library that are required for processing
+    These are the parts of the TADbit library that are required for processing
     FASTQ data. They have been packaged into chunks that can be easily handled
     by the COMPS infrastructure.
     
@@ -54,7 +55,7 @@ class fastq2adjacency:
         
         self.hic_data = None
 
-    def set_params(self, genome_accession, dataset, sra_id, library, enzyme_name, resolution, tmp_dir, data_dir, same_fastq=True, windows1=None, windows2=None):
+    def set_params(self, genome_accession, dataset, sra_id, library, enzyme_name, resolution, tmp_dir, data_dir, expt_name = None, same_fastq=True, windows1=None, windows2=None):
         self.genome_accession = genome_accession
         self.dataset     = dataset
         self.sra_id      = sra_id
@@ -64,29 +65,77 @@ class fastq2adjacency:
 
         self.temp_root = tmp_dir
         self.data_root = data_dir
-    
-        self.gem_file    = self.data_root + self.genome_accession + '/' + self.genome_accession + '.gem'
-        self.genome_file = self.data_root + self.genome_accession + '/chroms/' + self.genome_accession + '.fa'
         
-        self.fastq_file_1  = self.data_root + self.dataset + '/' + self.library + '/'
-        self.fastq_file_2  = self.data_root + self.dataset + '/' + self.library + '/'
-        if same_fastq == True:
-            self.fastq_file_1  = self.fastq_file_1 + self.sra_id + '.fastq'
-            self.fastq_file_2  = self.fastq_file_2 + self.sra_id + '.fastq'
+        self.gem_file = self.data_root + self.genome_accession + "/" + self.genome_accession + ".gem"
+        self.genome_accession = self.data_root + self.genome_accession + "/chroms/" + self.genome_accession + ".gem"
+        
+        if expt_name != None:
+            self.expt_name = expt_name + '/'
         else:
-            self.fastq_file_1  = self.fastq_file_1 + self.sra_id + '_1.fastq'
-            self.fastq_file_2  = self.fastq_file_2 + self.sra_id + '_2.fastq'
-            
-        self.map_dir     = self.data_root + self.dataset + '/' + self.library + '/01_it-mapped_read'
-        self.tmp_dir     = self.temp_root + self.dataset + '/' + self.library
+            self.expt_name = ''
+        
+        self.library_dir = self.data_root + self.expt_name + self.dataset + '/' + self.library + '/'
+        if same_fastq == True:
+            self.fastq_file_1  = self.library_dir + self.sra_id + '.fastq'
+            self.fastq_file_2  = self.library_dir + self.sra_id + '.fastq'
+        else:
+            self.fastq_file_1  = self.library_dir + self.sra_id + '_1.fastq'
+            self.fastq_file_2  = self.library_dir + self.sra_id + '_2.fastq'
+        
+        if expt_name != None:
+            self.expt_name = expt_name + '/'
+        else:
+            self.expt_name = ''
+        
+        
+        self.map_dir     = self.library_dir + '01_it-mapped_read'
+        self.tmp_dir     = self.temp_root + self.expt_name + self.dataset + '/' + self.library
         self.parsed_reads_dir = self.tmp_dir + '/parsed_reads'
         
-        mkdir(self.parsed_reads_dir)
+        try:
+            os.makedirs(self.map_dir)
+            os.makedirs(self.tmp_dir)
+            os.makedirs(self.parsed_reads_dir)
+        except:
+            pass
         
         if windows1 != None:
             self.windows1 = windows1
         if windows2 != None:
             self.windows2 = windows2
+    
+    def getFastqData(self):
+        f_index = urllib2.urlopen(
+        'http://www.ebi.ac.uk/ena/data/warehouse/filereport?accession=' + str(self.sra_id) + '&result=read_run&fields=study_accession,run_accession,tax_id,scientific_name,instrument_model,library_layout,fastq_ftp&download=txt')
+        data = f_index.read()
+        rows = data.split("\n")
+        row_count = 0
+        for row in rows:
+            if row_count == 0:
+                row_count += 1
+                continue
+            
+            row = row.rstrip()
+            row = row.split("\t")
+            
+            if len(row) == 0:
+                continue
+            
+            project = row[0]
+            srr_ir = row[1]
+            fastq_files = row[6].split(';')
+            row_count += 1
+            
+            for fastq_file in fastq_files:
+                file_name = fastq_file.split("/")
+                print fastq_file
+                print self.data_root + self.expt_name + self.dataset + '/' + self.library + '/' + file_name[-1]
+                print file_name[-1]
+                dl_file = open(self.data_root + self.expt_name + self.dataset + '/' + self.library + '/' + file_name[-1], "w")
+                dl = urllib2.urlopen("ftp://" + fastq_file)
+                dl_file.write(dl.read())
+                dl_file.close()
+        
     
     def mapWindows(self, side=1):
         """
@@ -136,8 +185,6 @@ class fastq2adjacency:
         mapped_rN = self.getMappedWindows()
 
         print 'Parse MAP files...'
-        print reads1
-        print reads2
         parse_map(mapped_rN["mapped_r1"], mapped_rN["mapped_r2"], out_file1=reads1, out_file2=reads2, genome_seq=self.genome_seq, re_name=self.enzyme_name, verbose=True, ncpus=8)
     
     def mergeMaps(self):
@@ -170,12 +217,57 @@ class fastq2adjacency:
             # Less conservative option
             apply_filter(reads, filt_reads, masked, filters=[1,2,3,9,10])
     
-    def load_hic_data(self):
+    def merge_adjacency_data(self, adjacency_matrixes):
+        """
+        The recommended route is to merge the normalised data is to sum the
+        normalised values from previous steps. This should be the final step of
+        the initial phase for main function should have finished by normalising
+        each of the individual adjacency files.
+        """
+        merged_matrix = Chromosome(name=self.expt_name, centromere_search=True)
+        merged_matrix.add_experiment(self.expt_name, resolution=self.resolution)
+        merged_exp = merged_matrix.experiment[self.expt_name]
+        print adjacency_data
+        
+        for m in adjacency_matrixes:
+            new_chrom = Chromosome(name=self.expt_name, centromere_search=True)
+            new_chrom.add_experiment(self.expt_name, hic_data=m, resolution=self.resolution)
+            merged_exp = merged_exp + new_chrom.experiment[self.expt_name]
+    
+    def generate_tads(self):
+        """
+        Uses TADbit to generate the TAD borders based on the computed hic_data
+        """
+        my_chrom = Chromosome(name=self.expt_name, centromere_search=True)
+        my_chrom.add_experiment(self.expt_name, hic_data=self.hic_data, resolution=self.resolution)
+        
+        # Run core TADbit function to find TADs on each expt.
+        # For the current dataset required 61GB of RAM
+        my_chrom.find_tad(self.expt_name, n_cpus=8)
+        
+        exp = my_chrom.experiments[self.expt_name]
+        tad_file = self.library_dir + expt_name + '_tads_' + str(resolution) + '.tsv'
+        tad_out = open(tad_file)
+        tad_out.write(exp.write_tad_borders())
+        tad_out.close()
+    
+    def load_hic_read_data(self):
         """
         Load the interactions into the HiC-Data data type
         """
         filter_reads = self.parsed_reads_dir + '/filtered_map.tsv'
         self.hic_data = load_hic_data_from_reads(filter_reads, resolution=self.resolution)
+    
+    def load_hic_matrix_data(self, norm=True):
+        """
+        """
+        if norm == True:
+            # Dump the data pre-normalized
+            adj_list = self.parsed_reads_dir + '/adjlist_map.tsv'
+        else:
+            adj_list = self.parsed_reads_dir + '/adjlist_map_norm.tsv'
+        
+        self.hic_data = read_matrix(adj_list, resolution=self.resolution)
     
     def normalise_hic_data(self, iterations=0):
         """

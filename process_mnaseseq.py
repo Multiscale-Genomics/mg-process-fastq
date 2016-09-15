c1f408358b9f150bdad8f77083167523115f9720
Mark McDowall <mark.mcdowall@gmail.com>
2016-09-15 15:29:00 +0100

    Includedd the shutil module

--- process_mnaseseq.py
+++ process_mnaseseq.py
@@ -16,7 +16,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import argparse, urllib2, gzip, shlex, subprocess, os.path
+import argparse, urllib2, gzip, shutil, shlex, subprocess, os.path
 
 from pycompss.api.task import task
 from pycompss.api.parameter import *

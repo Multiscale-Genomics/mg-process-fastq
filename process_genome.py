bf60922c39a5404d38488b86dcef2a4770f5b65a
Mark McDowall <mark.mcdowall@gmail.com>
2016-12-15 16:16:59 +0000

    Each indexer tool now has a skeleton runner. Outputs need defining, main workflow run function needs setting

--- process_genome.py
+++ process_genome.py
@@ -39,30 +39,89 @@ except ImportError :
     print "[Error] Cannot import \"pysam\" package. Have you installed it?"
     exit(-1)
 
-class indexerTool(Tool):
+class bowtieIndexerTool(Tool):
     """
     Tool for running indexers over a genome FASTA file
     """
     
     @task()
-    def bowtie2_indexer():
+    def bowtie2_indexer(self, file_loc):
         """
         Bowtie2 Indexer
         """
+        cf = common()
+        cf.bowtie_index_genome(file_loc)
+        return True
     
+    def run(self, input_files, metadata):
+         """
+        Standard function to call a task
+        """
+        
+        # handle error
+        if not self.bowtie_indexer(input_files[0]):
+            output_metadata.set_exception(
+                Exception(
+                    "macs2_peak_calling: Could not process files {}, {}.".format(*input_files)))
+output_file = None
+        return ([output_file], [output_metadata])
+    
+
+class bwaIndexerTool(Tool):
+    """
+    Tool for running indexers over a genome FASTA file
+    """
     
     @task()
     def bwa_indexer():
         """
         BWA Indexer
         """
+        cf = common()
+        cf.bwa_index_genome(file_loc)
+        return True
     
+    def run(self, input_files, metadata):
+         """
+        Standard function to call a task
+        """
+        
+        # handle error
+        if not self.bwa_indexer(input_files[0]):
+            output_metadata.set_exception(
+                Exception(
+                    "macs2_peak_calling: Could not process files {}, {}.".format(*input_files)))
+output_file = None
+        return ([output_file], [output_metadata])
+    
+    
+class bowtieIndexerTool(Tool):
+    """
+    Tool for running indexers over a genome FASTA file
+    """
     
     @task()
     def gem_indexer():
         """
         GEM Indexer
         """
+        cf = common()
+        cf.gem_index_genome(file_loc)
+        return True
+    
+    
+    def run(self, input_files, metadata):
+         """
+        Standard function to call a task
+        """
+        
+        # handle error
+        if not self.gem_indexer(input_files[0]):
+            output_metadata.set_exception(
+                Exception(
+                    "macs2_peak_calling: Could not process files {}, {}.".format(*input_files)))
+output_file = None
+        return ([output_file], [output_metadata])
 
 
 class processs_genome(Workflow):
@@ -73,4 +132,63 @@ class processs_genome(Workflow):
     should be prevented from running the indexing step as this will be done as
     part of this workflow.
     """
+    
+    def run(self, file_ids, metadata):
+        """
+        Main run function
+        """
+
+
+if __name__ == "__main__":
+    import sys
+    import os
+    
+    from pycompss.api.api import compss_wait_on
+    
+    # Set up the command line parameters
+    parser = argparse.ArgumentParser(description="ChIP-seq peak calling")
+    parser.add_argument("--species", help="Species (homo_sapiens)")
+    parser.add_argument("--genome", help="Genome FASTA file")
+    parser.add_argument("--file", help="Project ID of the dataset")
+    parser.add_argument("--bgd_file", help="Project ID of the dataset")
+    
+    # Get the matching parameters from the command line
+    args = parser.parse_args()
+    
+    species     = args.species
+    genome_fa   = args.genome
+    file_loc    = args.data_dir
+    file_bg_loc = args.tmp_dir
+    
+    pcs = process_chipseq()
+    cf = common()
+    
+    #
+    # MuG Tool Steps
+    # --------------
+    # 
+    # 1. Create data files
+    
+    # Get the assembly
+    genome_fa = cf.getGenomeFromENA(data_dir, species, assembly, False)
+    
+    #2. Register the data with the DMP
+    from dmp import dmp
+    
+    da = dmp()
+    
+    print da.get_files_by_user("test")
+    
+    genome_file = da.set_file("test", genome_fa, "fasta", "Assembly", 9606, None)
+    file_in = da.set_file("test", file_loc, "fasta", "ChIP-seq", 9606, None)
+    file_bg_in = da.set_file("test", file_bg_loc, "fasta", "ChIP-seq", 9606, None)
+    
+    print da.get_files_by_user("test")
+    
+    # 3. Instantiate and launch the App
+    from nnn import WorkflowApp
+    app = WoekflowApp()
+    results = app.launch(process_chipseq, [genome_file, file_in, file_bg_in], {})
+    
+    print da.get_files_by_user("test")
     

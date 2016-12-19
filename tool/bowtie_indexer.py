c1e970b13d3c2918395a83d04449edad27892ec7
Mark McDowall <mark.mcdowall@gmail.com>
2016-12-19 13:14:57 +0000

    Added licences

--- tool/bowtie_indexer.py
+++ tool/bowtie_indexer.py
@@ -1,3 +1,19 @@
+"""
+.. Copyright 2016 EMBL-European Bioinformatics Institute
+ 
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at 
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
 import os
 
 from pycompss.api.parameter import FILE_IN, FILE_OUT
@@ -7,6 +23,8 @@ from basic_modules.metadata import Metadata
 from basic_modules.tool import Tool
 
 from .. import common
+
+# ------------------------------------------------------------------------------
 
 class bowtieIndexerTool(Tool):
     """
@@ -34,3 +52,5 @@ class bowtieIndexerTool(Tool):
                     "bowtie2_indexer: Could not process files {}, {}.".format(*input_files)))
 output_file = None
         return ([output_file], [output_metadata])
+
+# ------------------------------------------------------------------------------
--- tool/bwa_indexer.py
+++ tool/bwa_indexer.py
@@ -1,3 +1,19 @@
+"""
+.. Copyright 2016 EMBL-European Bioinformatics Institute
+ 
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at 
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
 import os
 
 from pycompss.api.parameter import FILE_IN, FILE_OUT
@@ -8,18 +24,20 @@ from basic_modules.tool import Tool
 
 from .. import common
 
+# ------------------------------------------------------------------------------
+
 class bwaIndexerTool(Tool):
     """
     Tool for running indexers over a genome FASTA file
     """
     
-    @task(file_loc=FILE_IN, amb_loc=FILE_OUT, ann_loc=FILE_OUT, pac_loc=FILE_OUT)
+    @task(file_loc=FILE_IN, amb_loc=FILE_OUT, ann_loc=FILE_OUT, bwt_loc=FILE_OUT, pac_loc=FILE_OUT)
     def bwa_indexer(self, file_loc):
         """
         BWA Indexer
         """
         cf = common()
-        amb_loc, ann_loc, pac_loc = cf.bwa_index_genome(file_loc)
+        amb_loc, ann_loc, bwt_loc, pac_loc = cf.bwa_index_genome(file_loc)
         return True
     
     def run(self, input_files, metadata):
@@ -34,3 +52,5 @@ class bwaIndexerTool(Tool):
                     "bwa_indexer: Could not process files {}, {}.".format(*input_files)))
 output_file = None
         return ([output_file], [output_metadata])
+
+# ------------------------------------------------------------------------------

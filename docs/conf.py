819b464686dea2781b7233e46eae7ed034c70362
Mark McDowall <mark.mcdowall@gmail.com>
2017-02-28 16:29:09 +0000

    Removed requirement in the for sphinx github pages

--- docs/conf.py
+++ docs/conf.py
@@ -40,10 +40,11 @@ sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.viewcode'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

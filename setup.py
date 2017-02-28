d81d8d2a351180e28c8910f73684425d23076611
Mark McDowall <mark.mcdowall@gmail.com>
2017-02-28 15:24:21 +0000

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='mg-process-tsv',
-    packages=['mg-process-tsv'],
+    name='mg-process-fastq',
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'numpy', 'h5py', 'scipy', 'matplotlib', 'pysam', 'MACS2', 'rpy2'

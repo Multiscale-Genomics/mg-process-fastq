e45c2dbc17fbbfedbe8957004215f6c6c52307e7
Mark McDowall <mark.mcdowall@gmail.com>
2017-02-28 13:43:08 +0000

    Created the setup scripts for easy installation

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='mg-process-tsv',
+    packages=['mg-process-tsv'],
+    include_package_data=True,
+    install_requires=[
+        'numpy', 'h5py', 'scipy', 'matplotlib', 'TADbit', 'pysam', 'MACS2', 'rpy2'
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)

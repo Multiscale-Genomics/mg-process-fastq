b37938a7919359f865ba6e8cc7a66b284827f089
Mark McDowall <mark.mcdowall@gmail.com>
2017-03-30 16:05:11 +0100

    Changes to match those made for the ChIP-Seq pipelines wrt handling of the alignments with BWA

--- process_mnaseseq.py
+++ process_mnaseseq.py
@@ -60,13 +60,25 @@ class process_mnaseseq(Workflow):
         outputfiles : list
             List of locations for the output bam, bed and tsv files
         """
+
+        genome_fa = file_ids[0]
+        bwa_amb = file_ids[1]
+        bwa_ann = file_ids[2]
+        bwa_bwt = file_ids[3]
+        bwa_pac = file_ids[4]
+        bwa_sa  = file_ids[5]
+        file_loc = file_ids[6]
+        file_bgd_loc = file_ids[7]
         
         cwa_align = tool.bwaAlignerTool(self.configuration)
-        out_bam, out_meta = cwa_align.run((files_ids[0], files_ids[1]), ())
+        out_bam, out_meta = cwa_align.run(
+            [genome_fa, file_bgd_loc, out_bgd_bam, bwa_amb, bwa_ann, bwa_bwt, bwa_pac, bwa_sa],
+            {}
+        )
         
         # Needs moving to its own tool
         inps = tool.inps(self.configuration)
-        out_peak_bed, out_meta = inps.inps_peak_calling(out_bam, ())
+        out_peak_bed, out_meta = inps.inps_peak_calling(out_bam, {})
         
         return (out_peak_bed[0])
 
@@ -80,14 +92,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Mnase-seq peak calling")
     parser.add_argument("--assembly", help="Genome assembly ID (GCA_000001635.2)")
     parser.add_argument("--taxon_id", help="Taxon ID (10090)")
-    parser.add_argument("--species", help="Species (Mus_musculus)")
     parser.add_argument("--genome", help="Genome assembly FASTA file")
     parser.add_argument("--file", help="Location of FASTQ file")
     
     # Get the matching parameters from the command line
     args = parser.parse_args()
     
-    species    = args.species
     genome_fa  = args.genome
     taxon_id   = args.taxon_id
     assembly   = args.assembly
@@ -98,12 +108,22 @@ if __name__ == "__main__":
     print da.get_files_by_user("test")
     
     genome_file = da.set_file("test", genome_fa, "fasta", "Assembly", taxon_id, meta_data={'assembly' : assembly})
+    genome_file_idx1 = da.set_file("test", genome_fa + ".amb", "fasta", "Assembly", taxon_id, meta_data={'assembly' : assembly})
+    genome_file_idx2 = da.set_file("test", genome_fa + ".ann", "fasta", "Assembly", taxon_id, meta_data={'assembly' : assembly})
+    genome_file_idx3 = da.set_file("test", genome_fa + ".bwt", "fasta", "Assembly", taxon_id, meta_data={'assembly' : assembly})
+    genome_file_idx4 = da.set_file("test", genome_fa + ".pac", "fasta", "Assembly", taxon_id, meta_data={'assembly' : assembly})
+    genome_file_idx5 = da.set_file("test", genome_fa + ".sa", "fasta", "Assembly", taxon_id, meta_data={'assembly' : assembly})
     file_in = da.set_file("test", file_loc, "fastq", "Mnase-seq", taxon_id, meta_data={'assembly' : assembly})
     
     print da.get_files_by_user("test")
 
     files = [
         genome_fa,
+        genome_fa + ".amb",
+        genome_fa + ".ann",
+        genome_fa + ".bwt",
+        genome_fa + ".pac",
+        genome_fa + ".sa",
         file_loc
     ]
     

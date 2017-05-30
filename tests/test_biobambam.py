9d500474e6fff7ae1cedabd71aa2c0d42b2c21a4
Mark McDowall <mark.mcdowall@gmail.com>
2017-05-30 14:30:44 +0100

    Added the EMBL-EBI copyright to the top of the test scripts

--- tests/test_biobambam.py
+++ tests/test_biobambam.py
@@ -1,4 +1,20 @@
-#!usr/bin/python
+#!/usr/bin/python
+
+"""
+.. Copyright 2017 EMBL-European Bioinformatics Institute
+ 
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at 
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
 
 import pytest
 import random
@@ -11,7 +27,3 @@ def test_biobambam():
     resource_path = os.path.join(os.path.dirname(__file__), "data/")
     
     bbb.biobambam_filter_alignments(resource_path+"fastQForSelRegion.bam",resource_path+"_output_test.bam",resource_path)
-    
-    
-    
-test_biobambam()

cc55cd77e7f208d3e879d8e1a45b4da866aa277b
Mark McDowall <mark.mcdowall@gmail.com>
2017-06-06 09:25:19 +0100

    Modified the code so that it can run on python 2.7 and 3.5+

--- basic_modules/tool.py
+++ basic_modules/tool.py
@@ -1,6 +1,14 @@
 # from mug import datatypes as mug_datatypes
-from pycompss.api.parameter import FILE_IN, FILE_OUT
-from pycompss.api.task import task
+
+try:
+    from pycompss.api.parameter import FILE_IN, FILE_OUT
+    from pycompss.api.task import task
+except ImportError :
+    print("[Warning] Cannot import \"pycompss\" API packages.")
+    print("          Using mock decorators.")
+    
+    from dummy_pycompss import *
+
 from basic_modules.metadata import Metadata
 
 

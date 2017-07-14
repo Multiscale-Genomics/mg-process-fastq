dbce9d3c21cb21464f22d62970c20e49183bc71c
rehamFatima <reham@ebi.ac.uk>
2017-07-14 10:08:41 +0100

    Create setup_docker_biobambam.sh
    testing if docker can work with an .sh file as interactive mode is not working out

---
+++ setup_docker_biobambam.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /RehamCode/bin
+ln -s ${HOME}/lib/biobambam2/bin/bamcat bamcat
+ln -s ${HOME}/lib/biobambam2/bin/bamexplode bamexplode
+ln -s ${HOME}/lib/biobambam2/bin/bam12split bam12split

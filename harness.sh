4e370478c35fd7845fef68161c641de52c1c4d4a
rehamFatima <reham@ebi.ac.uk>
2017-07-18 16:42:37 +0100

    Update harness.sh

--- harness.sh
+++ harness.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pytest test_bwa_indexer.py
-pytest test_bwa_aligner.py
-pytest test_biobambam.py
+pytest tests/test_bwa_indexer.py
+pytest tests/test_bwa_aligner.py
+pytest tests/test_biobambam.py

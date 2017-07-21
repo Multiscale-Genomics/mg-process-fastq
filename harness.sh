0ccb91b0ebc5a9aadca01c5949e48bfa35d6b92d
rehamFatima <reham@ebi.ac.uk>
2017-07-21 17:05:21 +0100

    Update harness.sh

--- harness.sh
+++ harness.sh
@@ -2,4 +2,6 @@
 
 pytest -s tests/test_bwa_indexer.py
 pytest -s tests/test_bwa_aligner.py 
+exec ls /home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data
+exec bamsormadup --tmpfile="/home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data" </home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data/macs2.Human.DRR000150.22.bam >testFile.f.bam
 pytest -s tests/test_biobambam.py

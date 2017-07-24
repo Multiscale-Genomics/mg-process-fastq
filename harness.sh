cafd3ec8091623295bf115b5630c9c0562c80318
Andrew Yates <ayates@ebi.ac.uk>
2017-07-24 16:33:23 +0100

    Adding BAM and sliming down tests

--- harness.sh
+++ harness.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-pytest -s tests/test_bwa_indexer.py
-pytest -s tests/test_bwa_aligner.py 
+# pytest -s tests/test_bwa_indexer.py
+# pytest -s tests/test_bwa_aligner.py 
 
-echo "before bams, after tests"
-ls -l /home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data
+# echo "before bams, after tests"
+# ls -l /home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data
 
-bgzip --help
-htsfile --help
-tabix --help
+# bgzip --help
+# htsfile --help
+# tabix --help
 
 echo $PATH
 set -x

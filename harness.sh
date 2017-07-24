5fa3c135b097431fa3f14ee20b52cf9b1d251ca8
rehamFatima <reham@ebi.ac.uk>
2017-07-24 14:40:39 +0100

    Update harness.sh

--- harness.sh
+++ harness.sh
@@ -7,6 +7,9 @@ echo "before bams, after tests"
 ls -l /home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data
 
 bgzip --help
+htsfile --help
+tabix --help
+
 
 exec bamsormadup --tmpfile="/home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data" </home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data/macs2.Human.DRR000150.22.bam >testFile.f.bam 
 echo "after bams"

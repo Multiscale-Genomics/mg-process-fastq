9159173b9169d260112e80411987cf2fce83ae2f
Andrew Yates <ayates@ebi.ac.uk>
2017-07-24 16:59:31 +0100

    Switching on all tests after possible fix.
    We may have fixed bamsormadup by not assigning a pseudo-TTY to the run image and also made sure the container is destroyed after being run. Turning on the  commands that were active at the branch point

--- harness.sh
+++ harness.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-# pytest -s tests/test_bwa_indexer.py
-# pytest -s tests/test_bwa_aligner.py 
+pytest -s tests/test_bwa_indexer.py
+pytest -s tests/test_bwa_aligner.py 
 
-# echo "before bams, after tests"
-# ls -l /home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data
+echo "before bams, after tests"
+ls -l /home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data
 
-# bgzip --help
-# htsfile --help
-# tabix --help
+bgzip --help
+htsfile --help
+tabix --help
 
 echo $PATH
-set -x
 bamsormadup --tmpfile="/home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data" </home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data/macs2.Human.DRR000150.22.bam >testFile.f.bam 
-set +x
 echo "after bams"
 ls /home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data
 ls -ltra $PWD
 
-# pytest -s tests/test_biobambam.py
-# echo "after bams test"
-# ls /home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data
+pytest -s tests/test_biobambam.py
+echo "after bams test"
+ls /home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data

29d2da506e16cda5324a806b3d64ce372da56c8f
Mark McDowall <mark.mcdowall@gmail.com>
2017-07-25 11:44:17 +0100

    Minor changes for clarity

--- split_paired_fastq.py
+++ split_paired_fastq.py
@@ -16,11 +16,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import argparse, os.path, sys
+import argparse
 
 from fastqreader import fastqreader
-    
-        
+
+
 # Set up the command line parameters
 parser = argparse.ArgumentParser(description="Load adjacency list into HDF5 file")
 parser.add_argument("--input_1", help="File 1")
@@ -33,7 +33,7 @@ file2 = args.input_2
 tag = args.output_tag
 
 fqr = fastqreader()
-fqr.openPairedFastQ(file1, file2)
+fqr.openFastQ(file1, file2)
 fqr.createOutputFiles(tag)
 
 r1 = fqr.next(1)
@@ -46,14 +46,14 @@ count_r3 = 0
 while fqr.eof(1) == False and fqr.eof(2) == False:
     r1_id = r1["id"].split(" ")
     r2_id = r2["id"].split(" ")
-    
+
     if r1_id[0] == r2_id[0]:
         fqr.writeOutput(r1, 1)
         fqr.writeOutput(r2, 2)
-        
+
         r1 = fqr.next(1)
         r2 = fqr.next(2)
-        
+
         count_r1 += 1
         count_r2 += 1
         count_r3 += 1
@@ -63,12 +63,12 @@ while fqr.eof(1) == False and fqr.eof(2) == False:
     else:
         r2 = fqr.next(2)
         count_r2 += 1
-    
+
     if count_r3 % 1000000 == 0:
         print count_r1, r1_id[0], count_r2, r2_id[0], count_r3
         fqr.incrementOutputFiles()
 
-fqr.closePairedFastQ()
+fqr.closeFastQ()
 fqr.closeOutputFiles()
 
 print count_r1, r1_id[0], count_r2, r2_id[0], count_r3

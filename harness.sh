f12e55552e5c91f644aa47cdba6cb7c918ef7a5b
rehamFatima <reham@ebi.ac.uk>
2017-07-25 09:46:25 +0100

    Update harness.sh

--- harness.sh
+++ harness.sh
@@ -2,19 +2,14 @@
 
 pytest -s tests/test_bwa_indexer.py
 pytest -s tests/test_bwa_aligner.py 
-
-echo "before bams, after tests"
-ls -l /home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data
-
-bgzip --help
-htsfile --help
-tabix --help
-
-
-bamsormadup --tmpfile="/home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data" </home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data/macs2.Human.DRR000150.22.bam >testFile.f.bam 
-echo "after bams"
-ls /home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data
-
 pytest -s tests/test_biobambam.py
-echo "after bams test"
-ls -l /home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data
+pytest -s tests/test_bs_seeker_indexer.py
+pytest -s tests/test_bs_seeker_aligner.py
+pytest -s tests/test_bs_seeker_filter.py
+pytest -s tests/test_bs_seeker_methylation_caller.py
+pytest -s tests/test_bowtie_indexer.py
+pytest -s tests/test_kallisto_indexer.py
+pytest -s tests/test_kallisto_quant.py
+pytest -s tests/test_macs2.py
+pytest -s tests/test_paired_splitter.py
+pytest -s tests/test_single_splitter.py

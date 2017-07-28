adb66f3eac464fea7c23f20b9a866fb36c09ca1e
rehamFatima <reham@ebi.ac.uk>
2017-07-28 11:31:51 +0100

    Update harness.sh

--- harness.sh
+++ harness.sh
@@ -7,9 +7,7 @@ pytest  tests/test_bs_seeker_indexer.py
 pytest  tests/test_bs_seeker_aligner.py
 pytest  tests/test_bs_seeker_filter.py
 pytest  tests/test_bs_seeker_methylation_caller.py
-ls /home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data
 pytest -s tests/test_bowtie_indexer.py
-ls /home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data
 pytest -s tests/test_kallisto_indexer.py
 pytest -s tests/test_kallisto_quant.py
 pytest -s tests/test_macs2.py

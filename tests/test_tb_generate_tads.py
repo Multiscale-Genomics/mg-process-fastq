70c68995fdae3eb21c4fca788be41a6fa531500e
Mark McDowall <mark.mcdowall@gmail.com>
2017-07-28 14:41:35 +0100

    Added tests for each of the frag and iter windowing with different levels of filtering for calling TADs

--- tests/test_tb_generate_tads.py
+++ tests/test_tb_generate_tads.py
@@ -26,11 +26,86 @@ def test_tb_parse_mapping_frag_01():
     Test case to ensure that the BWA indexer works.
     """
     resource_path = os.path.join(os.path.dirname(__file__), "data/")
-    reads_tsv = resource_path + "tb.Human.SRR1658573_filtered_map.tsv"
+    reads_tsv = resource_path + "tb.Human.SRR1658573_frag_01_filtered_map.tsv"
 
     metadata = {
         'assembly' : 'test',
-        'expt_name' : 'tb.Human.SRR1658573_frag',
+        'expt_name' : 'tb.Human.SRR1658573_frag_01',
+        'enzyme_name' : 'MboI',
+        'windows' : ((1, 'end')),
+        'mapping' : ['frag', 'frag'],
+        'resolutions' : [10000, 100000],
+        'normalized' : False
+    }
+
+    tgt_handle = tbGenerateTADsTool()
+    tgt_files, tgt_meta = tgt_handle.run([reads_tsv], [], metadata)
+
+    print(tgt_files)
+
+    assert os.path.isfile(tgt_files[0]) is True
+    assert os.path.getsize(tgt_files[0]) > 0
+
+def test_tb_parse_mapping_frag_02():
+    """
+    Test case to ensure that the BWA indexer works.
+    """
+    resource_path = os.path.join(os.path.dirname(__file__), "data/")
+    reads_tsv = resource_path + "tb.Human.SRR1658573_frag_02_filtered_map.tsv"
+
+    metadata = {
+        'assembly' : 'test',
+        'expt_name' : 'tb.Human.SRR1658573_frag_02',
+        'enzyme_name' : 'MboI',
+        'windows' : ((1, 'end')),
+        'mapping' : ['frag', 'frag'],
+        'resolutions' : [10000, 100000],
+        'normalized' : False
+    }
+
+    tgt_handle = tbGenerateTADsTool()
+    tgt_files, tgt_meta = tgt_handle.run([reads_tsv], [], metadata)
+
+    print(tgt_files)
+
+    assert os.path.isfile(tgt_files[0]) is True
+    assert os.path.getsize(tgt_files[0]) > 0
+
+def test_tb_parse_mapping_iter_01():
+    """
+    Test case to ensure that the BWA indexer works.
+    """
+    resource_path = os.path.join(os.path.dirname(__file__), "data/")
+    reads_tsv = resource_path + "tb.Human.SRR1658573_iter_01_filtered_map.tsv"
+
+    metadata = {
+        'assembly' : 'test',
+        'expt_name' : 'tb.Human.SRR1658573_iter_01',
+        'enzyme_name' : 'MboI',
+        'windows' : ((1, 'end')),
+        'mapping' : ['frag', 'frag'],
+        'resolutions' : [10000, 100000],
+        'normalized' : False
+    }
+
+    tgt_handle = tbGenerateTADsTool()
+    tgt_files, tgt_meta = tgt_handle.run([reads_tsv], [], metadata)
+
+    print(tgt_files)
+
+    assert os.path.isfile(tgt_files[0]) is True
+    assert os.path.getsize(tgt_files[0]) > 0
+
+def test_tb_parse_mapping_iter_02():
+    """
+    Test case to ensure that the BWA indexer works.
+    """
+    resource_path = os.path.join(os.path.dirname(__file__), "data/")
+    reads_tsv = resource_path + "tb.Human.SRR1658573_iter_02_filtered_map.tsv"
+
+    metadata = {
+        'assembly' : 'test',
+        'expt_name' : 'tb.Human.SRR1658573_iter_02',
         'enzyme_name' : 'MboI',
         'windows' : ((1, 'end')),
         'mapping' : ['frag', 'frag'],

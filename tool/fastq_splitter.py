80945d4bd420a626d06e631cd0bf882e22b1c992
Mark McDowall <mark.mcdowall@gmail.com>
2017-08-08 16:54:34 +0100

    Updated the handling of naming of tmp files

--- tool/fastq_splitter.py
+++ tool/fastq_splitter.py
@@ -207,15 +207,13 @@ class fastq_splitter(Tool):
             if count_r3 % 1000000 == 0:
                 fqr.incrementOutputFiles()
                 file_loc_1 = fqr.fastq1.split("/")
-                file_loc_1[-1] = file_loc_1[-1].replace(
-                    ".fastq",
-                    "." + str(fqr.output_tag) + "_" + str(fqr.output_file_count) + ".fastq")
+                new_suffix = "." + str(fqr.output_tag) + "_" + str(fqr.output_file_count) + ".fastq"
+                file_loc_1[-1] = re.sub('.fastq$', new_suffix, file_loc_1[-1])
                 file_loc_1.insert(-1, "tmp")
 
                 file_loc_2 = fqr.fastq2.split("/")
-                file_loc_2[-1] = file_loc_2[-1].replace(
-                    ".fastq",
-                    "." + str(fqr.output_tag) + "_" + str(fqr.output_file_count) + ".fastq")
+                new_suffix = "." + str(fqr.output_tag) + "_" + str(fqr.output_file_count) + ".fastq"
+                file_loc_2[-1] = re.sub('.fastq$', new_suffix, file_loc_2[-1])
                 file_loc_2.insert(-1, "tmp")
 
                 files_out.append([file_loc_1[-1], file_loc_2[-1]])

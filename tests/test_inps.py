473f83bd93605e0f80a9bde83f2455493d2c0041
Mark McDowall <mark.mcdowall@gmail.com>
2017-08-10 16:47:55 +0100

    Modified the way that the files are tested so that it checks the files that are returned by the function

--- tests/test_inps.py
+++ tests/test_inps.py
@@ -31,15 +31,10 @@ def test_inps():
     peak_bed = bam_file.replace('.bam', '.bed')
 
     inps_obj = inps.inps()
-    inps_obj.run([bam_file, peak_bed], {})
+    inps_files, inps_meta = inps_obj.run([bam_file, peak_bed], {})
 
-    assert os.path.isfile(resource_path + "inps.Mouse.DRR000386.bam.bed") is True
-    assert os.path.getsize(resource_path + "inps.Mouse.DRR000386.bam.bed") > 0
-    assert os.path.isfile(resource_path + "inps.Mouse.DRR000386.bed_19.like_bed") is True
-    assert os.path.getsize(resource_path + "inps.Mouse.DRR000386.bed_19.like_bed") > 0
-    assert os.path.isfile(resource_path + "inps.Mouse.DRR000386.bed_19.like_wig") is True
-    assert os.path.getsize(resource_path + "inps.Mouse.DRR000386.bed_19.like_wig") > 0
-
-    # All results final file
-    assert os.path.isfile(resource_path + "inps.Mouse.DRR000386.bed_Gathering.like_bed") is True
-    assert os.path.getsize(resource_path + "inps.Mouse.DRR000386.bed_Gathering.like_bed") > 0
+    # Add tests for all files created
+    for f_out in inps_files:
+        print("iNPS RESULTS FILE:", f_out)
+        assert os.path.isfile(f_out) is True
+        assert os.path.getsize(f_out) > 0

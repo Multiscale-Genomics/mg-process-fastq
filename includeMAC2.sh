b19aef752b3b5ade892815bd6f48e83fe51bfe03
rehamFatima <reham@ebi.ac.uk>
2017-08-11 17:13:54 +0100

    Update includeMAC2.sh

--- includeMAC2.sh
+++ includeMAC2.sh
@@ -4,5 +4,6 @@ python_version=$(python --version 2>&1)
 echo $python_version
 if [[ $python_version != *"3."* ]]; then
      cd ${HOME}/code
-     pip install MACS2    
+     pip install MACS2 
+     ln -s ${HOME}/.pyenv/versions/mg-process-fastq/bin/macs2 ${HOME}/bin/macs2
 fi

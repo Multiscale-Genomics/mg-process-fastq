c684c6ef08c11f992b9199861157467f491af721
rehamFatima <reham@ebi.ac.uk>
2017-08-14 17:09:03 +0100

    including TAD bit in conditional install

--- includeMAC2.sh
+++ includeMAC2.sh
@@ -6,4 +6,11 @@ if [[ $python_version != *"3."* ]]; then
      cd ${HOME}/code
      pip install MACS2 
      ln -s ${HOME}/.pyenv/versions/mg-process-fastq/bin/macs2 ${HOME}/bin/macs2
+     
+     cd ${HOME}/lib
+     wget https://github.com/3DGenomes/tadbit/archive/master.zip -O tadbit.zip
+     unzip tadbit.zip
+     cd TADbit-master
+     yes | python setup.py install --install-lib=${HOME}/.pyenv/versions/mg-process-fastq/lib/python2.7/site-packages/ --install-scripts=${HOME}/bin 
+    
 fi

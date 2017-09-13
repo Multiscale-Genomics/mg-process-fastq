cc7e4e5d6b477041a6b93eb99a1ebb704f3ae141
rehamFatima <reham@ebi.ac.uk>
2017-09-13 17:14:50 +0100

    Delete setup_docker_biobambam.sh

--- setup_docker_biobambam.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /RehamCode/bin
-ln -s ${HOME}/lib/biobambam2/bin/bamcat bamcat
-ln -s ${HOME}/lib/biobambam2/bin/bamexplode bamexplode
-ln -s ${HOME}/lib/biobambam2/bin/bam12split bam12split

6dfa35c61b94cdb01c89bd253b000d3ae0de3edb
Mark McDowall <mark.mcdowall@gmail.com>
2017-10-18 09:45:26 +0100

    Removed the iNPS checking and updated the harness.sh script to use the test_toolchains.py script

--- harness.sh
+++ harness.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
-pytest  tests/test_bwa_indexer.py
-pytest  tests/test_bwa_aligner.py 
-pytest  tests/test_biobambam.py
-pytest  tests/test_bs_seeker_indexer.py
-pytest  tests/test_bs_seeker_aligner.py
-pytest  tests/test_bs_seeker_filter.py
-samtools
-ls /home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data
-pytest  tests/test_bs_seeker_methylation_caller.py
-pytest -s tests/test_bowtie_indexer.py
-pytest -s tests/test_kallisto_indexer.py
-pytest -s tests/test_kallisto_quant.py
-pytest -s tests/test_macs2.py
-pytest -s tests/test_paired_splitter.py
-pytest -s tests/test_single_splitter.py
+python tests/test_toolchains.py --pipeline genome
+./tidy_data.sh
 
-python_version=$(python --version 2>&1)
+python tests/test_toolchains.py --pipeline chipseq
+./tidy_data.sh
 
-echo $python_version
-if [[ $python_version == *"3."* ]]; then
-    pytest -s tests/test_inps.py
-fi
+python tests/test_toolchains.py --pipeline rnaseq
+./tidy_data.sh
+
+python tests/test_toolchains.py --pipeline wgbs
+./tidy_data.sh
+
+# pytest  tests/test_bwa_indexer.py
+# pytest  tests/test_bwa_aligner.py
+# pytest  tests/test_biobambam.py
+# pytest  tests/test_bs_seeker_indexer.py
+# pytest  tests/test_bs_seeker_aligner.py
+# pytest  tests/test_bs_seeker_filter.py
+# samtools
+# ls /home/travis/build/Multiscale-Genomics/mg-process-fastq/tests/data
+# pytest  tests/test_bs_seeker_methylation_caller.py
+# pytest -s tests/test_bowtie_indexer.py
+# pytest -s tests/test_kallisto_indexer.py
+# pytest -s tests/test_kallisto_quant.py
+# pytest -s tests/test_macs2.py
+# pytest -s tests/test_paired_splitter.py
+# pytest -s tests/test_single_splitter.py
+
+# python_version=$(python --version 2>&1)
+
+# echo $python_version
+# if [[ $python_version == *"3."* ]]; then
+#     pytest -s tests/test_inps.py
+# fi

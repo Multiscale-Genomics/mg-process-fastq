98fe2500e1476f5a52f2c06da01e6676f5c60fb6
Mark McDowall <mark.mcdowall@gmail.com>
2017-10-20 10:18:46 +0100

    Removed reference to the pyenv for MACS2 using alias

--- scripts/travis/includeMAC2.sh
+++ scripts/travis/includeMAC2.sh
@@ -20,10 +20,9 @@ echo $python_version
 if [[ $python_version != *"3."* ]]; then
     cd ${HOME}/code
     pip install MACS2
-    ln -s ${HOME}/.pyenv/versions/mg-process-fastq/bin/macs2 ${HOME}/bin/macs2
 else
     cd ${HOME}/code
     pip install MACS2 == MACS2p3
-    ln -s ${HOME}/.pyenv/versions/mg-process-fastq/bin/macs2p3 ${HOME}/bin/macs2
+    alias macs2="macs2p3"
 
 fi

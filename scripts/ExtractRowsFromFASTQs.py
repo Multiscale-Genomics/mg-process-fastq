1ffaadb18efc238cba0068f193465e506aad3f88
Mark McDowall <mark.mcdowall@gmail.com>
2018-01-11 11:09:08 +0000

    Added single FASTQ file handling

--- scripts/ExtractRowsFromFASTQs.py
+++ scripts/ExtractRowsFromFASTQs.py
@@ -6,6 +6,7 @@ FASTQ IDs.
 """
 
 import argparse
+import time
 
 from fastqreader import fastqreader
 
@@ -99,6 +100,72 @@ def paired_selector(in_file1, in_file2, rows, tag='tmp'):
 
     return files_out
 
+def single_selector(in_file1, rows, tag='tmp'):
+    """
+    Function to divide the FastQ files into separate sub files of 1000000
+    sequences so that the aligner can run in parallel.
+
+    Parameters
+    ----------
+    in_file1 : str
+        Location of first paired end FASTQ file
+    in_file2 : str
+        Location of second paired end FASTQ file
+    tag : str
+        DEFAULT = tmp
+        Tag used to identify the files. Useful if this is getting run
+        manually on a single machine multiple times to prevent collisions of
+        file names
+
+
+    Returns
+    -------
+    Returns: Returns a list of lists of the files that have been generated.
+             Each sub list containing the two paired end files for that
+             subset.
+    paired_files : list
+        List of lists of pair end files. Each sub list containing the two
+        paired end files for that subset.
+    """
+
+    fqr = fastqreader()
+    fqr.openFastQ(in_file1)
+    fqr.createOutputFiles(tag)
+
+    current_milli_time = lambda: int(round(time.time() * 1000))
+
+    time_1 = current_milli_time()
+
+    record1 = fqr.next()
+
+    file_loc_1 = fqr.fastq1.split("/")
+    file_loc_1[-1] = file_loc_1[-1].replace(
+        ".fastq",
+        "." + str(fqr.output_tag) + "_" + str(fqr.output_file_count) + ".fastq")
+    file_loc_1.insert(-1, tag)
+
+    files_out = [["/".join(file_loc_1)]]
+
+    counter = 0
+
+    while fqr.eof() is False and counter < 1000:
+        r1_id = record1["id"].split(" ")
+
+        if r1_id[0][1:] in rows:
+            fqr.writeOutput(record1)
+            counter += 1
+
+            if counter % 1000 == 0:
+                time_2 = current_milli_time()
+                print("Extracted:", str(counter), "reads (avg per 1000: " + str(time_2-time_1) + ") ...")
+                time_1 = time_2
+
+        record1 = fqr.next()
+
+    fqr.closeFastQ()
+    fqr.closeOutputFiles()
+
+    return files_out
 
 def get_if_list(row_file):
     """
@@ -122,7 +189,7 @@ if __name__ == "__main__":
     # Set up the command line parameters
     PARSER = argparse.ArgumentParser(description="Load adjacency list into HDF5 file")
     PARSER.add_argument("--input_1", help="File 1")
-    PARSER.add_argument("--input_2", help="File 2")
+    PARSER.add_argument("--input_2", required=False, default=None, help="File 2")
     PARSER.add_argument("--rows", help="Row File")
     PARSER.add_argument("--output_tag", help="Inserted before the file descriptor and after the file name: e.g. 'matching' would convert file_id-1.fastq to file_id-1.matching.fastq")
 
@@ -134,4 +201,9 @@ if __name__ == "__main__":
 
     VALID_IDS = get_if_list(ROW_FILE)
 
-    paired_selector(FILE_01, FILE_02, VALID_IDS, TAG)
+    print(FILE_01, FILE_02, ROW_FILE, TAG)
+
+    if FILE_02 is not None:
+        paired_selector(FILE_01, FILE_02, VALID_IDS, TAG)
+    else:
+        single_selector(FILE_01, VALID_IDS, TAG)

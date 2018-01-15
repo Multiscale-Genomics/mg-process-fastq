d9b6678a5dc1934360bd16bbb8042eb5c7cb2ef0
Mark McDowall <mark.mcdowall@gmail.com>
2018-01-15 13:41:10 +0000

    Updated the tool chain test script

--- tests/test_toolchains.py
+++ tests/test_toolchains.py
@@ -98,7 +98,7 @@ def idamidseq_toolchain(verbose=False):
     params.append('tests/test_bwa_aligner.py')
     params.append('tests/test_biobambam.py')
     params.append('tests/test_bsgenome.py')
-    # params.append('tests/test_idear.py')
+    params.append('tests/test_idear.py')
 
     return pytest.main(params)
 

e2eca94413f724f79cbe17e93dfeb6b8516d33c2
Mark McDowall <mark.mcdowall@gmail.com>
2018-05-17 14:32:47 +0100

    Tidied up some legacy code

--- scripts/travis/install_code_test_dependencies.sh
+++ scripts/travis/install_code_test_dependencies.sh
@@ -83,22 +83,6 @@ wget https://github.com/arq5x/bedtools2/releases/download/v2.27.1/bedtools-2.27.
 tar -zxvf bedtools-2.27.1.tar.gz
 cd bedtools2
 make
-# cd ${HOME}/build/Multiscale-Genomics/mg-process-fastq
-
-# Install MACS2
-# cd ${HOME}/build/Multiscale-Genomics/mg-process-fastq
-# chmod +x scripts/travis/includeMAC2.sh
-# ./scripts/travis/includeMAC2.sh
-
-# Install R packages required by iDEAR
-# cd ${HOME}/build/Multiscale-Genomics/mg-process-fastq
-# chmod +x scripts/travis/install_packages.R
-# sudo Rscript scripts/install_packages.R
-
-# Install TADbit (only on Python 2.7)
-# cd ${HOME}/build/Multiscale-Genomics/mg-process-fastq
-# chmod +x scripts/travis/includeTADbit.sh
-# ./scripts/travis/includeTADbit.sh
 
 # Post Installation Tidyup
 cd ${HOME}/lib

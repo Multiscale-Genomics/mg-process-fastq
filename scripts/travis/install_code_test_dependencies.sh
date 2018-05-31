c5b9559bfcd50c50467e6cb203b8186e0bed1d1d
Mark McDowall <mark.mcdowall@gmail.com>
2018-05-31 12:09:38 +0100

    Moved the RNA-seq and Sleuth to a spearate travis testset so that the R related tests are handled separately

--- scripts/travis/install_code_test_dependencies.sh
+++ scripts/travis/install_code_test_dependencies.sh
@@ -74,9 +74,9 @@ wget http://www.picb.ac.cn/hanlab/files/iNPS_V1.2.2.zip
 unzip iNPS_V1.2.2.zip
 
 # Kallisto
-cd ${HOME}/lib
-wget https://github.com/pachterlab/kallisto/releases/download/v0.43.1/kallisto_linux-v0.43.1.tar.gz
-tar -xzf kallisto_linux-v0.43.1.tar.gz
+# cd ${HOME}/lib
+# wget https://github.com/pachterlab/kallisto/releases/download/v0.43.1/kallisto_linux-v0.43.1.tar.gz
+# tar -xzf kallisto_linux-v0.43.1.tar.gz
 
 # bedTools
 wget https://github.com/arq5x/bedtools2/releases/download/v2.27.1/bedtools-2.27.1.tar.gz
---
+++ scripts/travis/r_harness.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+rc=0
+pv=$(python -c 'import platform; print(platform.python_version())')
+
+python tests/test_toolchains.py --pipeline rnaseq
+tc=$?
+rc=$(($rc + $tc))
+bash tidy_data.sh
+
+python tests/test_pipelines.py --pipeline rnaseq
+tc=$?
+rc=$(($rc + $tc))
+bash tidy_data.sh
+
+python tests/test_toolchains.py --pipeline sleuth
+tc=$?
+rc=$(($rc + $tc))
+bash tidy_data.sh
+
+python tests/test_pipelines.py --pipeline sleuth
+tc=$?
+rc=$(($rc + $tc))
+bash tidy_data.sh
+
+if [[ $rc != 0 ]]; then exit $rc; fi

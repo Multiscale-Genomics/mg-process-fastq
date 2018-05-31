d39d66033395aeeb126ac52e37ec725dfdc10b81
Mark McDowall <mark.mcdowall@gmail.com>
2018-05-31 10:10:53 +0100

    Changes to handle the _public data along with changes to the ChIP-seq test and a description in the ADR for the work

--- tests/test_pipeline_chipseq.py
+++ tests/test_pipeline_chipseq.py
@@ -24,9 +24,23 @@ from basic_modules.metadata import Metadata
 from process_chipseq import process_chipseq
 
 
+def tidy_data():
+    """
+    Runs the tidy_data.sh script
+    """
+    print("TIDY DATA")
+    try:
+        command_line = './tidy_data.sh'
+        process = subprocess.Popen(command_line, shell=True)
+        process.wait()
+    except (IOError, OSError) as msg:
+        print("I/O error({0}): {1}\n{2}".format(
+            msg.errno, msg.strerror, command_line))
+
+
 @pytest.mark.chipseq
 @pytest.mark.pipeline
-def test_chipseq_pipeline():
+def test_chipseq_pipeline_00():
     """
     Test case to ensure that the ChIP-seq pipeline code works.
 
@@ -91,3 +105,82 @@ def test_chipseq_pipeline():
         # assert chipseq_files[f_out] == files_out[f_out]
         assert os.path.isfile(chipseq_files[f_out]) is True
         assert os.path.getsize(chipseq_files[f_out]) > 0
+
+        try:
+            os.remove(chipseq_files[f_out])
+        except OSError, ose:
+            print("Error: %s - %s." % (ose.filename, ose.strerror))
+
+
+@pytest.mark.chipseq
+@pytest.mark.pipeline
+def test_chipseq_pipeline_01():
+    """
+    Test case to ensure that the ChIP-seq pipeline code works.
+
+    Running the pipeline with the test data from the command line:
+
+    .. code-block:: none
+
+       runcompss                                                         \\
+          --lang=python                                                  \\
+          --library_path=${HOME}/bin                                     \\
+          --pythonpath=/<pyenv_virtenv_dir>/lib/python2.7/site-packages/ \\
+          --log_level=debug                                              \\
+          process_chipseq.py                                             \\
+             --taxon_id 9606                                             \\
+             --genome /<dataset_dir>/Human.GCA_000001405.22.fasta        \\
+             --assembly GRCh38                                           \\
+             --file /<dataset_dir>/DRR000150.22.fastq
+
+    """
+    resource_path = os.path.join(os.path.dirname(__file__), "data/")
+
+    files = {
+        'genome_public': resource_path + 'macs2.Human.GCA_000001405.22.fasta',
+        'loc': resource_path + 'macs2.Human.DRR000150.22.fastq',
+        'index_public': resource_path + 'macs2.Human.GCA_000001405.22.fasta.bwa.tar.gz'
+    }
+
+    metadata = {
+        "genome_public": Metadata(
+            "Assembly", "fasta", files['genome_public'], None,
+            {'assembly': 'GCA_000001405.22'}),
+        "loc": Metadata(
+            "data_chip_seq", "fastq", files['loc'], None,
+            {'assembly': 'GCA_000001405.22'}
+        ),
+        "index_public": Metadata(
+            "Index", "bwa_index", files['index_public'], files['genome_public'],
+            {'assembly': 'GCA_000001405.22', "tool": "bwa_indexer"}),
+    }
+
+    root_name = files['loc'].split("/")
+    root_name[-1] = root_name[-1].replace('.fastq', '')
+
+    files_out = {
+        "bam": files['loc'].replace(".fastq", ".bam"),
+        "filtered": files['loc'].replace(".fastq", "_filtered.bam"),
+        "output": files['loc'].replace(".fastq", ".tsv"),
+        'narrow_peak': '/'.join(root_name) + '_filtered_peaks.narrowPeak',
+        'summits': '/'.join(root_name) + '_filtered_summits.bed',
+        'broad_peak': '/'.join(root_name) + '_filtered_peaks.broadPeak',
+        'gapped_peak': '/'.join(root_name) + '_filtered_peaks.gappedPeak'
+    }
+
+    chipseq_handle = process_chipseq({"macs_nomodel_param": True})
+    chipseq_files, chipseq_meta = chipseq_handle.run(files, metadata, files_out)  # pylint: disable=unused-variable
+
+    print(chipseq_files)
+
+    # Add tests for all files created
+    for f_out in chipseq_files:
+        print("CHIP-SEQ RESULTS FILE:", f_out)
+        # assert chipseq_files[f_out] == files_out[f_out]
+        assert os.path.isfile(chipseq_files[f_out]) is True
+        assert os.path.getsize(chipseq_files[f_out]) > 0
+
+        try:
+            os.remove(chipseq_files[f_out])
+        except OSError, ose:
+            print("Error: %s - %s." % (ose.filename, ose.strerror))

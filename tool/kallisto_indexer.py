9c13ee0c2e4acf4cf2672cbfdb796dd54dbb61b9
Mark McDowall <mark.mcdowall@gmail.com>
2018-06-22 14:43:24 +0100

    There is no reliance on compss_wait_on

--- tool/kallisto_indexer.py
+++ tool/kallisto_indexer.py
@@ -28,14 +28,12 @@ try:
         raise ImportError
     from pycompss.api.parameter import FILE_IN, FILE_OUT
     from pycompss.api.task import task
-    # from pycompss.api.api import compss_wait_on
 except ImportError:
     logger.warn("[Warning] Cannot import \"pycompss\" API packages.")
     logger.warn("          Using mock decorators.")
 
     from utils.dummy_pycompss import FILE_IN, FILE_OUT  # pylint: disable=ungrouped-imports
     from utils.dummy_pycompss import task  # pylint: disable=ungrouped-imports
-    # from utils.dummy_pycompss import compss_wait_on  # pylint: disable=ungrouped-imports
 
 from basic_modules.tool import Tool
 from basic_modules.metadata import Metadata
@@ -118,7 +116,6 @@ class kallistoIndexerTool(Tool):
             input_files["cdna"],
             output_files["index"]
         )
-        # results = compss_wait_on(results)
 
         output_metadata = {
             "index": Metadata(
--- tool/kallisto_quant.py
+++ tool/kallisto_quant.py
@@ -30,14 +30,12 @@ try:
         raise ImportError
     from pycompss.api.parameter import FILE_IN, FILE_OUT
     from pycompss.api.task import task
-    # from pycompss.api.api import compss_wait_on
 except ImportError:
     logger.warn("[Warning] Cannot import \"pycompss\" API packages.")
     logger.warn("          Using mock decorators.")
 
     from utils.dummy_pycompss import FILE_IN, FILE_OUT  # pylint: disable=ungrouped-imports
     from utils.dummy_pycompss import task  # pylint: disable=ungrouped-imports
-    # from utils.dummy_pycompss import compss_wait_on  # pylint: disable=ungrouped-imports
 
 from basic_modules.tool import Tool
 from basic_modules.metadata import Metadata
@@ -258,13 +256,6 @@ class kallistoQuantificationTool(Tool):
 
         # input and output share most metadata
         output_metadata = {}
-
-        # file_loc = input_files[1].split("/")
-        # output_dir = "/".join(file_loc[0:-1])
-
-        # abundance_h5_file = output_dir + "/abundance.h5"
-        # abundance_tsv_file = output_dir + "/abundance.tsv"
-        # run_info_file = output_dir + "/run_info.json"
 
         if "fastq2" not in input_files:
             self.kallisto_quant_single(

ae27c25074d6ffc7c74e4af2965d949d64a78e0f
Mark McDowall <mark.mcdowall@gmail.com>
2018-09-17 09:40:25 +0100

    Pylint issues

--- tool/macs2.py
+++ tool/macs2.py
@@ -174,7 +174,8 @@ class macs2(Tool):  # pylint: disable=invalid-name
         common_handle.to_output_file(output_tmp.format(name, 'peaks.broadPeak'), broadpeak)
         common_handle.to_output_file(output_tmp.format(name, 'peaks.gappedPeak'), gappedpeak)
         common_handle.to_output_file(output_tmp.format(name, 'summits.bed'), summits_bed)
-        common_handle.to_output_file(output_tmp.format(name, 'control_lambda.bdg'), bdg_control_lambda)
+        common_handle.to_output_file(
+            output_tmp.format(name, 'control_lambda.bdg'), bdg_control_lambda)
         common_handle.to_output_file(output_tmp.format(name, 'treat_pileup.bdg'), bdg_treat_pileup)
 
         return True
@@ -376,7 +377,7 @@ class macs2(Tool):  # pylint: disable=invalid-name
 
         return command_params
 
-    def run(self, input_files, input_metadata, output_files):  # pylint: disable=too-many-locals
+    def run(self, input_files, input_metadata, output_files):  # pylint: disable=too-many-locals,too-many-branches,too-many-statements
         """
         The main function to run MACS 2 for peak calling over a given BAM file
         and matching background BAM file.

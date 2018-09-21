af32d2ee335e4248cf7c03404aa4f2240a137db5
Mark McDowall <mark.mcdowall@gmail.com>
2018-09-21 10:44:30 +0100

    Error in the linter when adding in the TADbit code

--- scripts/travis/pylint_harness.sh
+++ scripts/travis/pylint_harness.sh
@@ -18,7 +18,7 @@
 disabled="--disable=similarities,invalid-name,too-many-statements,too-many-arguments,too-many-locals,too-few-public-methods,relative-import,no-self-use,,useless-object-inheritance"
 
 pylint ${disabled} --rcfile pylintrc process*.py > output.err
-pylint ${disabled} --rcfile pylintrc *wrapper*.py > output.err
+pylint ${disabled} --rcfile pylintrc *wrapper*.py >> output.err
 pylint ${disabled} --rcfile pylintrc tool >> output.err
 pylint ${disabled} --rcfile pylintrc tests >> output.err
 
